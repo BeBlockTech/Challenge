83660e6448e9b38a778062042a37d61e695eb433
unknown <arismendy.campusano@gmail.com>
2017-09-20 14:11:33 +0200

    The Hard Way

---
+++ Python/TheHardWay/ex1.py
@@ -0,0 +1,7 @@
+print"Hello World!"
+print"Hello Agail"
+print"I like typing this."
+print"This is fun."
+print'YAy! Printing.'
+print"I'd much rather you 'not'."
+print'I "said" do not touch this.'
---
+++ Python/TheHardWay/ex2.py
@@ -0,0 +1,8 @@
+# Now I need to restart all over
+# And start writing commrnts again
+
+print "I could have code like this." #and more comments
+
+# Bla bla  bla bla
+#and bla bla
+print"voila."
