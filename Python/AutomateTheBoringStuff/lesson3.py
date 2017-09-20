e1bb5ef846949a1e02855cda84dcf273c8a09d33
unknown <arismendy.campusano@gmail.com>
2017-09-20 14:19:25 +0200

    Automate it

---
+++ Python/AutomateTheBoringStuff/lesson3.py
@@ -0,0 +1,14 @@
+# This programs aasks for your name and says hello
+
+print('Hello world!')
+
+print('What is your name?') #Ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The lenght of your name is : ')
+print(len(myName))
+
+print('What is your age?') #Ask for their age
+yourAge = input()
+print('You will be ' + str(int(yourAge) + 1) + ' in a year.' )
+
