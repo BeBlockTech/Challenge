3122937b1af05cc0481cf7e897c317bb24155a26
unknown <arismendy.campusano@gmail.com>
2017-10-15 18:01:48 +0200

    Updated

--- Solidity/pet-tut/test/TestAdoption.sol
+++ Solidity/pet-tut/test/TestAdoption.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.11;
 
-import "truffle/assert.sol";
+import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 
@@ -9,10 +9,10 @@ contract TestAdoption {
 
 
 	// Testing the adopt() function
-	function testUserCanAdoptPet()() {
+	function testUserCanAdoptPet() {
 	uint returnedId = adoption.adopt(8);
 
-	uint expected 8;
+	uint expected = 8;
 
 	Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
 	}
@@ -23,6 +23,17 @@ contract TestAdoption {
 		address adopter = adoption.adopters(8);
 
 		Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+	}
+
+	// Testing retrieval of all owners
+	function testGetAdopterAddressByPetIdArray() {
+		// Expected owner is this contract 
+		address expected = this;
+
+		// Store adopters in memory rather than contract's storage
+		address[16] memory adopters = adoption.getAdopters();
+
+		Assert.equal(adopters[8], expected, "Owner of this pet ID 8 should be recorded.");
 	}
 		
 		
