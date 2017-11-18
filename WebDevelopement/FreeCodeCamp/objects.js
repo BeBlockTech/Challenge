61bb671e832b4c64e195776256170b19b02f1c19
aris comas <arismendy.campusano@gmail.com>
2017-11-19 00:11:07 +0100

    Objects

---
+++ WebDevelopement/FreeCodeCamp/objects.js
@@ -0,0 +1,20 @@
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Lol",
+  "legs": 1,
+  "tails": 1,
+  "friends": ["Food"]
+  
+  
+  
+};
