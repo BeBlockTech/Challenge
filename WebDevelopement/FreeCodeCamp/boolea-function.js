e0ee9167ec28c61b4c2269852fd767b1b96868b1
aris comas <arismendy.campusano@gmail.com>
2017-11-18 21:59:51 +0100

    Returning boolean values from fuctions

---
+++ WebDevelopement/FreeCodeCamp/boolea-function.js
@@ -0,0 +1,27 @@
+
+
+function isLess(a, b) {
+  // Fix this code
+ return a < b;
+}
+
+// Change these values to test
+isLess(15, 10);
+
+
+
+
+
+/*
+function isLess(a, b) {
+  // Fix this code
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Change these values to test
+isLess(10, 15);
+*/
