4e4b5918cecb63546d38a84889482981d91224de
aris comas <arismendy.campusano@gmail.com>
2017-11-19 21:04:54 +0100

    Manipulating objects

---
+++ WebDevelopement/FreeCodeCamp/manipulating-objects.js
@@ -0,0 +1,25 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Moi Meme", 
+    "title": "Je suis",
+    "release_year": 1981,
+    "formats": [
+      "CS",
+      "LP",
+      "9T"],
+    "gold" : false
+  }
+];
+
