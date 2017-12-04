e50f6d97d9313a18d835835198f4ccac862cadea
aris comas <arismendy.campusano@gmail.com>
2017-12-04 03:02:29 +0100

    Troubleshooting

--- Python/TheHardWay/ex13.py
+++ Python/TheHardWay/ex13.py
@@ -1,8 +1,10 @@
 from sys import argv
 
-script, first, second, third = argv
+script, first, second, third, fourth, raw_input = argv
 
 print ("The script is called:"), script
 print ("Your first variable is:"), first
 print ("Your second variable is:"), second
 print ("Your third variable is:"), third 
+print ("Your third variable is:"), fourth
+print ("Your input1 variable is:"), raw_input
