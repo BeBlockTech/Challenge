9338edb19186774397f52ac8be99eddec8cdf1bf
aris comas <arismendy.campusano@gmail.com>
2018-01-22 02:53:00 +0100

    I'm back...

---
+++ Python/AutomateTheBoringStuff/lesson2.py
@@ -0,0 +1,13 @@
+# Let's automate the boring stuff
+
+print("Hello world")
+print("What's your name?")
+myName = input()
+print("It's good to meet you, " + myName)
+print("The length of your name is:" )
+print(len(myName))
+print("What's your age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year." ) 
+      
+      
