4294d4d041c0a811b5a1103d0259e71564c2043e
aris comas <arismendy.campusano@gmail.com>
2018-01-23 22:50:38 +0100

    More...

---
+++ WebDevelopement/CodeCademy/forEachMethod.js
@@ -0,0 +1,14 @@
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(function(fruitsItem) {
+ console.log('I want to eat a ' + fruitsItem);
+               });
+
+// Same function with an arrow method
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(fruitsItem =>
+ console.log('I want to eat a ' + fruitsItem));
+
+
