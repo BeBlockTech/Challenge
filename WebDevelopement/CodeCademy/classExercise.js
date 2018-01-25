184ac2d346866bf1c230d629823441c60c197a7e
aris comas <arismendy.campusano@gmail.com>
2018-01-25 23:49:12 +0100

    WebDevelopement/CodeCademy/classExercise.js

---
+++ WebDevelopement/CodeCademy/classExercise.js
@@ -0,0 +1,23 @@
+class HospitalEmployee {
+  constructor(Doctor, Nurse) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+    this._insurance = insurance;
+  }
+  
+  get name() {
+    return this._name();
+  }
+  
+  get remainingVacationDays {
+    return this._vacationDays();
+  }
+  
+  get insurance() {
+    return this._insurance();
+  }
+
+
+
+
+}
